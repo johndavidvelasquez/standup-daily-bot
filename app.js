bc144d40089c32e2fa3009666d288ebadb8eb59d
johndavidvelasquez <johndavidvelaquezz@gmail.com>
2021-04-19 18:08:45 +0800

    simple updates

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+require('dotenv').config()
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
@@ -13,12 +14,30 @@ client.on('ready', () => {
 
 // Create an event listener for messages
 client.on('message', message => {
-  // If the message is "ping"
-  if (message.content === 'ping') {
-    // Send "pong" to the same channel
-    message.channel.send('pong');
+
+  const addTaskCommand = "add-task";
+
+  
+  if (message.content.includes(`${addTaskCommand.toLowerCase()} completed`)) {
+    message.author.send('pong completed');
   }
+
+  else if (message.content.includes(`${addTaskCommand.toLowerCase()} in-progress`)) {
+    message.author.send('pong in-progress');
+  }
+
+  else if(message.content.includes(`${addTaskCommand.toLowerCase()}`)) {
+    console.log(message.author.username);
+    message.author.send('tset');
+  }
+
+  if(message.channel.type == "dm")
+  {
+     message.author.send('nu yon?');
+  }
+
+
 });
 
 // Log our bot in using the token from https://discord.com/developers/applications
-client.login('ODMzNjI5MTA2ODkyOTYzODQw.YH1Hmw.B80Fx5BLhBGSIru0Zbu29RZT8eQ');
+client.login(process.env.DISCORD_TOKEN);

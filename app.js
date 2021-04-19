d785ab2ae3cf7ffbdf2350a7a4b2501e159b7641
johndavidvelasquez <johndavidvelaquezz@gmail.com>
2021-04-19 17:23:35 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
+});
+
+// Log our bot in using the token from https://discord.com/developers/applications
+client.login('ODMzNjI5MTA2ODkyOTYzODQw.YH1Hmw.B80Fx5BLhBGSIru0Zbu29RZT8eQ');
